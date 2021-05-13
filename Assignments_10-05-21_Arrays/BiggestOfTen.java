e2383f97549478264fae4f26a3929332adcbedbf
anilandra2511 <anilkumar.andra@ojas-it>
2021-05-13 15:59:36 +0530

    first commit

---
+++ Assignments_10-05-21_Arrays/BiggestOfTen.java
@@ -0,0 +1,42 @@
+package com.ojas.arrays;
+
+import java.util.Scanner;
+
+public class BiggestOfTen 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ten values : ");
+		int values[] = new int[10];
+		for(int i = 0 ; i < values.length ; i++)
+		{
+			values[i] = sc.nextInt();
+		}
+		displayNum(values);
+		System.out.println("\nand The Bigest value is  "+getBiggest(values));
+	}
+	static void displayNum(int nums[])
+	{
+		System.out.println("The given values are :");
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			System.out.print(nums[i]+" ");
+		}
+	}
+	
+	static int getBiggest(int numbers[])
+	{
+		int res = numbers[0];
+		for(int i = 0; i < numbers.length;i++)
+		{
+			if(numbers[i] > res)
+			{
+				res = numbers[i];
+			}
+		}
+		return res;
+	}
+
+}
---
+++ Assignments_10-05-21_Arrays/ReverseOrder.java
@@ -0,0 +1,41 @@
+package com.ojas.arrays;
+
+import java.util.Scanner;
+
+public class ReverseOrder 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ten values : ");
+		int values[] = new int[10];
+		for(int i = 0 ; i < values.length ; i++)
+		{
+			values[i] = sc.nextInt();
+		}
+		displayNum(values);
+		System.out.println("\nThe values in the reverse order are :\n"+ reverseOrdering(values));
+	}
+	
+	static void displayNum(int nums[])
+	{
+		System.out.println("The given values are :");
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			System.out.print(nums[i]+" ");
+		}
+		System.out.println("\n");
+	}
+	
+	static String reverseOrdering(int values[])
+	{
+		String res = "";
+		for(int i = (values.length - 1)  ; i >= 0 ; i--)
+		{
+			res += values[i]+" ";
+		}
+		return res;
+	}
+
+}
